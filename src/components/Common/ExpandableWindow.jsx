228ac51eec804781387461d761b6d2d9bbab1f27
rjarrin <jarrin.ricardo@gmail.com>
2024-05-26 18:20:29 -0400

    Implemented abstract ExpandableWindow for resuable information sections
    Introduced a generic ExpandableWindow component to encapsulate the expandable/collapsible functionality and styling.

---
+++ src/components/Common/ExpandableWindow.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import '../../styles/ExpandableWindow.css'
+
+function ExpandableWindow({title, children}) {
+    const [isExpanded, setIsExpanded] = useState(false);
+
+    function toggleExpansion() {
+        setIsExpanded(!isExpanded);
+    }
+
+    return (
+        <div className={`expandable-window ${isExpanded? 'expanded' : ''}`}>
+            <div className="title" onClick={toggleExpansion}>
+                <span>{title}</span>
+                <span className="arrow">{isExpanded? '▼' : '►'}</span>
+            </div>
+            {children}
+        </div>
+    );
+}
+
+export default ExpandableWindow;
--- src/components/GeneralInfo.jsx
+++ src/components/GeneralInfo.jsx
@@ -1,30 +1,40 @@
-import { useState } from "react";
+// import { useState } from "react";
+// import '../styles/GeneralInfo.css';
+
+// function GeneralInfo() {
+//     const [isExpanded, setIsExpanded] = useState(false);
+
+//     function toggleExpansion() {
+//         setIsExpanded(!isExpanded);
+//     }
+    
+//     return (
+//         <div className={`general-info ${isExpanded? 'expanded' : ''}`}>
+//             <div className="title"  onClick={toggleExpansion}>
+//                 <span>General Information</span>
+//                 <span className="arrow">{isExpanded? '▼' : '>'}</span>
+//             </div>
+//             <form>
+//                 <label htmlFor="name">Name:</label>
+//                 <input type="text" id="name" name="name" placeholder="Enter your name" />
+//             </form>
+//         </div>
+//     );
+// }
+
+// export default GeneralInfo;
+
+import ExpandableWindow from "./Common/ExpandableWindow";
 import '../styles/GeneralInfo.css';
 
 function GeneralInfo() {
-    const [isExpanded, setIsExpanded] = useState(false);
-
-    function toggleExpansion() {
-        setIsExpanded(!isExpanded);
-    }
-    
     return (
-        <div className={`general-info ${isExpanded? 'expanded' : ''}`}>
-            <div className="title"  onClick={toggleExpansion}>
-                <span>General Information</span>
-                <span className="arrow">{isExpanded? '▼' : '>'}</span>
-            </div>
-            {/* {isExpanded && (
-                <form>
-                    <label htmlFor="name">Name:</label>
-                    <input type="text" id="name" name="name" placeholder="Enter your name" />
-                </form>
-            )} */}
+        <ExpandableWindow title="General Information">
             <form>
                 <label htmlFor="name">Name:</label>
                 <input type="text" id="name" name="name" placeholder="Enter your name" />
             </form>
-        </div>
+        </ExpandableWindow>
     );
 }
 
